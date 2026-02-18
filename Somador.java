8ac35bd7dbee0120a02bdc2627dedfdcf55d8c47
victoriaevellym2009 <victoriaevellym2009@gmail.com>
2026-02-18 14:14:19 -0300

    primeiro commit

--- Somador.java
+++ Somador.java
@@ -1,10 +1,29 @@
 package aulaJPA.aula1;
 
+import java.util.Scanner;
+
 public class Somador {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+        // Variáveis
+		double a,b ,som;
+		//Instaciar classe Scanner (System.in);
+		Scanner ler = new Scanner  (System.in);
+		
+		// Entrada de dados
+		System.out.println("Informe valor 1: ");
+		a = ler.nextDouble();
+		
+		System.out.println("Informe valor 2: ");
+		b = ler.nextDouble();
+		
+		//Processamento
+		som = a+b;
+		//Sáida
+		System.out.println("O resultado é :" + som);
+		
+		
 	}
 
 }
