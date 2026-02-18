9f9a28eaef7b978e229c4565bce77964fb96d2c0
victoriaevellym2009 <victoriaevellym2009@gmail.com>
2026-02-18 12:08:57 -0300

    primeiro commit

---
+++ Principal.java
@@ -0,0 +1,12 @@
+package aulaJPA.aula1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// esse código printa o texto na tela
+		System.out.println("evellym");
+
+	}
+
+}
---
+++ Somador.java
@@ -0,0 +1,10 @@
+package aulaJPA.aula1;
+
+public class Somador {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
